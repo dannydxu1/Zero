0c0006a6d22aa2daf1106e8ddf1b3728b7b5614f
Danny Xu <danny.d.xu@gmail.com>
2022-04-01 10:34:38 -0500

    Initial Commit

---
+++ ZERO/ZEROApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZEROApp.swift
+//  ZERO
+//
+//  Created by 90308209 on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ZEROApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
